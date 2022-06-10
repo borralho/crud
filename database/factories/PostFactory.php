8626ed854fc5c8206668f0302b17706654edd978
raphaell@algoritme.com.br <mamita12>
2022-06-10 10:09:33 -0300

    Correcao da seed

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -18,7 +18,7 @@ class PostFactory extends Factory
     {
         return [
             'title' => $this->faker->sentence,
-            'body' => $this->faker->paragraph,
+            'body' => $this->faker->realText(250),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\Post::factory(10000)->create();
+         \App\Models\Post::factory(100)->create();
     }
 }
